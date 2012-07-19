ae00e272280e204e6f6b3ccbf1237311a73ee9f4
Kirill <Kirill89@gmail.com>
2012-07-19 17:42:15 +0400

    threads begin

--- TestProject/TestProject/Form1.cs
+++ TestProject/TestProject/Form1.cs
@@ -12,6 +12,7 @@ namespace TestProject
     public partial class Form1 : Form
     {
         GCTest mGCTest;
+        ThreadTest mThreadTest = new ThreadTest();
 
         public Form1()
         {
@@ -20,7 +21,7 @@ namespace TestProject
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Console.Beep();
+            //Console.Beep();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -36,6 +37,16 @@ namespace TestProject
         private void button3_Click(object sender, EventArgs e)
         {
             mGCTest = new GCTest();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mThreadTest.GetThreadPoolConfigs();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            mThreadTest.StartThreadSimpleTest();
         }
     }
 }
---
+++ TestProject/TestProject/ThreadTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TestProject
+{
+    class ThreadTest
+    {
+        public ThreadTest()
+        {
+            
+        }
+
+        public void StartThreadSimpleTest()
+        {
+            Console.WriteLine("work...");
+
+            ThreadPool.QueueUserWorkItem(AsyncJob, "work from thread...");
+
+            Console.WriteLine("work again...");
+        }
+
+        private static void AsyncJob(Object state)
+        {
+            Console.WriteLine(state.ToString());
+        }
+
+        public void GetThreadPoolConfigs()
+        {
+            int maxc, maxw;
+            ThreadPool.GetMaxThreads(out maxw, out maxc);
+
+            Console.WriteLine("MAX\tw:" + maxw.ToString() + " c: " + maxc.ToString());
+
+            int minc, minw;
+            ThreadPool.GetMinThreads(out minw, out minc);
+            Console.WriteLine("MIN\tw:" + minw.ToString() + " c: " + minc.ToString());
+
+        }
+    }
+}
